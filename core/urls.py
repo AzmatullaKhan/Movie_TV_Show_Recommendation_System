b9e1c94bcdbe5dc5c170607109dc8c800e373d53
klu_2200031618 <kk258786@gmail.com>
2024-01-14 16:55:39 +0530

    Made all the required pages and Did some coding for navbar

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('', views.landing, name="landing"),
-    path('home', views.home, name='home')
+    path('home', views.home, name='home'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
+    path('signin', views.signin, name='signin'),
+    path('signup', views.signup, name='signup'),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,11 @@ def landing(request):
     return render(request, 'landing.html')
 def home(request):
     return render(request, 'home.html')
+def contact(request):
+    return render(request, 'contact.html')
+def about(request):
+    return render(request, 'about.html')
+def signin(request):
+    return render(request, 'registration/signin.html')
+def signup(request):
+    return render(request, 'registration/signup.html')
