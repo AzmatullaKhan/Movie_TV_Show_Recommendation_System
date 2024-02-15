c7ca8bd7e49eb2ec94e26bc691cce66e9bc10430
klu_2200031618 <kk258786@gmail.com>
2024-02-15 11:05:04 +0530

    All the categories that need to be added in hidden container are added and need to complete reset password page

--- core/data.py
+++ core/data.py
@@ -1,15 +1,37 @@
-data_line1=[ {'name': 'hidden1','id':'movie1','genre':'Action','url':'../static/images/movies/movie1.jpg'},
-    {'name': 'hidden2','id':'movie2','genre':'Romance & Fiction','url':'../static/images/movies/movie2.jpg'},
-    {'name': 'hidden3','id':'movie3','genre':'Action','url':'../static/images/movies/movie3.jpg'},
-    {'name': 'hidden4','id':'movie4','genre':'Action','url':'../static/images/movies/movie4.jpg'},
-    {'name': 'hidden5','id':'movie5','genre':'Action','url':'../static/images/movies/movie5.jpg'},
-    # {'name': 'hidden4','id':'movie4','genre':'drama'},
-    # {'name': 'hidden5','id':'movie5','genre':'fantasy'},
-    # {'name': 'hidden6','id':'movie6','genre':'mystery'},
-    # {'name': 'hidden7','id':'movie7','genre':'romance'},
-    # {'name': 'hidden8','id':'movie8','genre':'thriller'},
-    # {'name': 'hidden9','id':'movie9','genre':'documentary'},
-    # {'name': 'hidden10','id':'movie10','genre':'science-fiction'},
-    # {'name': 'hidden11','id':'movie11','genre':'historical'},
-    # {'name': 'hidden12','id':'movie12','genre':'crime'},
+data_line1=[ 
+    {
+        'name': 'hidden1','id':'movie1','genre':'Action, Crime & Drama','url':'../static/images/movies/movie1.jpg', 
+        'desc':'The fate of a violently contested kingdom hangs on the fraught bond between two friends-turned-foes in this saga of power, bloodshed and betrayal.',
+        'director':'Prashanth Neel',
+        'writers':'Sandeep Reddy Bandla, Choudary Hanuman & Prashanth Neel',
+        'streaming':'Netflix'
+    },
+    {
+        'name': 'hidden2','id':'movie2','genre':'Romance & Drama','url':'../static/images/movies/movie2.jpg',
+        'desc':'Love blooms between a doctor with an uncertain future and a renowned palmist who thought he would never find love. His beliefs are challenged.',
+        'director':'K.K. Radhakrishna Kumar',
+        'writers':'Abbas Dalal, Hussain Dalal & Madhan Karky',
+        'streaming':'Netflix, Prime Video & Zee5'
+     },
+    {
+        'name': 'hidden3','id':'movie3','genre':'Action & Thriller','url':'../static/images/movies/movie3.jpg',
+        'desc':'An undercover cop becomes embroiled in a battle with warring criminals who want to acquire a "black box," their ultimate key to a treasure.',
+        'director':'Sujeeth',
+        'writers':'Sujeeth, K.G.R Ashok & Abbas Dalal',
+        'streaming':'Netflix & Prime Video'    
+    },
+    {
+        'name': 'hidden4','id':'movie4','genre':'Action & Drama','url':'../static/images/movies/movie4.jpg',
+        'desc':'A child from the Mahishmati kingdom is raised by tribal people and one day learns about his royal heritage, his fathers bravery in battle and a mission to overthrow the incumbent ruler.',
+        'director':'S.S. Rajamouli',
+        'writers':'Vijayendra PrasadS.S, RajamouliC.H & Vijay Kumar',
+        'streaming':'Netflix, Diney+ Hotstar & Sony Liv'
+    },
+    {
+        'name': 'hidden5','id':'movie5','genre':'Action & Drama','url':'../static/images/movies/movie5.jpg',
+        'desc':'Amarendra Baahubali, the heir apparent to the throne of Mahishmati, finds his life and relationships endangered as his adoptive brother Bhallaladeva conspires to claim the throne.',
+        'director':'S.S. Rajamouli',
+        'writers':'Vijayendra PrasadS.S, RajamouliC.H & Vijay Kumar',
+        'streaming':'Netflix, Diney+ Hotstar & Sony Liv'
+    },
 ]
