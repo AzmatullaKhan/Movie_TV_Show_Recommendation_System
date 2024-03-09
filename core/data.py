255c441f9dcee0d7b4b112891396278f32c9c0ee
klu_2200031618 <kk258786@gmail.com>
2024-03-10 03:10:36 +0530

    Search bar logic is intact and working efficiently

--- core/data.py
+++ core/data.py
@@ -1,5 +1,6 @@
 data_line1=[ 
-    {
+    {   
+        'movie_name':'salar',
         'name': 'hidden1','id':'movie1','genre':'Action, Crime & Drama','url':'../static/images/movies/movie1.jpg', 
         'desc':'The fate of a violently contested kingdom hangs on the fraught bond between two friends-turned-foes in this saga of power, bloodshed and betrayal.',
         'director':'Prashanth Neel',
@@ -8,6 +9,7 @@ data_line1=[
         'index':1,
     },
     {
+        'movie_name':'radhe shyam',
         'name': 'hidden2','id':'movie2','genre':'Romance & Drama','url':'../static/images/movies/movie2.jpg',
         'desc':'Love blooms between a doctor with an uncertain future and a renowned palmist who thought he would never find love. His beliefs are challenged.',
         'director':'K.K. Radhakrishna Kumar',
@@ -16,6 +18,7 @@ data_line1=[
         'index':2,
      },
     {
+        'movie_name':'saaho',
         'name': 'hidden3','id':'movie3','genre':'Action & Thriller','url':'../static/images/movies/movie3.jpg',
         'desc':'An undercover cop becomes embroiled in a battle with warring criminals who want to acquire a "black box," their ultimate key to a treasure.',
         'director':'Sujeeth',
@@ -24,6 +27,7 @@ data_line1=[
         'index':3,
     },
     {
+        'movie_name':'bahubali 1',
         'name': 'hidden4','id':'movie4','genre':'Action & Drama','url':'../static/images/movies/movie4.jpg',
         'desc':'A child from the Mahishmati kingdom is raised by tribal people and one day learns about his royal heritage, his fathers bravery in battle and a mission to overthrow the incumbent ruler.',
         'director':'S.S. Rajamouli',
@@ -32,6 +36,7 @@ data_line1=[
         'index':4,
     },
     {
+        'movie_name':'bahubali 2',
         'name': 'hidden5','id':'movie5','genre':'Action & Drama','url':'../static/images/movies/movie5.jpg',
         'desc':'Amarendra Baahubali, the heir apparent to the throne of Mahishmati, finds his life and relationships endangered as his adoptive brother Bhallaladeva conspires to claim the throne.',
         'director':'S.S. Rajamouli',
--- core/static/js/nav.js
+++ core/static/js/nav.js
@@ -2,3 +2,26 @@ let logo=document.getElementById('logo')
 logo.addEventListener('click', ()=>{
     window.location.href='http://127.0.0.1:8888/home'
 })
+function Search(e){
+    const search=e.target.value.toUpperCase();
+    const items=document.getElementById('mini-main')
+    const product=document.querySelectorAll(".main-container")
+    const pname=items.getElementsByTagName("h2");
+    for(var i=0;i<pname.length;i++){
+        let match=pname[i];
+        if(match){
+            let textValue=match.innerHTML
+            if(textValue.toUpperCase().indexOf(search)>-1){
+                product[i].style.display=""
+                document.getElementById('main-container').style.display="none"
+            }
+            else{
+                product[i].style.display="none"
+                document.getElementById('main-container').style.display="none"
+            }
+            if(search===""){
+                document.getElementById('main-container').style.display=""
+            }
+        }
+    }
+}
