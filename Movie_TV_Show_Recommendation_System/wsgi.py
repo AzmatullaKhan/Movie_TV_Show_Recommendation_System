f200a135bcff7aa107a53863a4a4e061c0fa9cfe
klu_2200031618 <kk258786@gmail.com>
2024-03-10 10:01:23 +0530

    Committing for Deployment

--- Movie_TV_Show_Recommendation_System/wsgi.py
+++ Movie_TV_Show_Recommendation_System/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Movie_TV_Show_Recommendation_System.settings')
 
 application = get_wsgi_application()
+
+app=application
